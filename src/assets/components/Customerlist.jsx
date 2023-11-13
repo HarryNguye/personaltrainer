642102f69f57ba6b43f11b02e56f3edb9dc28263
HarryNguye <harrynguyen100603@gmail.com>
2023-11-13 13:06:02 +0200

    Fetch toimii molemmilla tabilla

--- src/assets/components/Customerlist.jsx
+++ src/assets/components/Customerlist.jsx
@@ -1,11 +1,75 @@
-function Customerlist() {
+import React, { useEffect, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+//import EditCustomer from "./EditCustomer"; // Oletan, että sinulla on tämä komponentti
 
-    const AsiakasFetch = () =>{
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
+function CustomerList() {
+  const [customers, setCustomers] = useState([]);
+
+
+
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
+
+  const [columnDefs] = useState([
+    { field: 'firstname', sortable: true, filter: true },
+    { field: 'lastname', sortable: true, filter: true },
+    { field: 'streetaddress', sortable: true, filter: true },
+    { field: 'postcode', sortable: true, filter: true },
+    { field: 'city', sortable: true, filter: true },
+    { field: 'email', sortable: true, filter: true },
+    { field: 'phone', sortable: true, filter: true },
+  ]);
+
+  const fetchCustomers = () => {
+    fetch('http://traineeapp.azurewebsites.net/api/customers')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Virhe tietojen hakemisessa");
+      }
+    })
+    .then(data => {
+      if (Array.isArray(data.content)) {
+        setCustomers(data.content);
+        console.log(data)
+      } else {
+        throw new Error("Haettu data ei ole taulukko");
+      }
+  })
+    .catch(error => {
+      console.error("Virhe:", error);
+  })}
+
+  const deleteCustomer = (url) => {
+    if (window.confirm("Are you sure you want to delete this customer?")) {
+      fetch(url, { method: 'DELETE' })
+        .then(response => {
+          if (response.ok) {
+            // Päivittää asiakaslistan poiston jälkeen
+            fetchCustomers();
+          } else {
+            throw new Error("Error in DELETE: " + response.statusText);
+          }
+        })
+        .catch(err => console.error(err));
     }
+  };
 
-    return <h2>Asiakkaat</h2>;
-    // Add your list code here
-  }
-  export default Customerlist;
-  
+  return (
+    <div className="ag-theme-material" style={{ height: 500 }}>
+      <h2>Asiakaslista</h2>
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowData={customers}
+        animateRows={true}>
+      </AgGridReact>
+    </div>
+  );
+}
+
+export default CustomerList;
--- src/assets/components/Traininglist.jsx
+++ src/assets/components/Traininglist.jsx
@@ -1,11 +1,82 @@
-function Traininglist() {
+import React, { useEffect, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import Button from '@mui/material/Button';
+// import EditTraining from "./EditTraining"; // Oletan, että sinulla on vastaava komponentti harjoituksille
 
-    const TreeniFetch = () =>{
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
+function TrainingList() {
+  const [trainings, setTrainings] = useState([]);
+
+  useEffect(() => {
+    fetchTrainings(); // Tämä funktio lataa harjoitustiedot
+  }, []);
+
+  const [columnDefs] = useState([
+    { field: 'date', sortable: true, filter: true }, // Esimerkiks harjoituksen päivämäärä
+    { field: 'duration', sortable: true, filter: true }, 
+    { field: 'activity', sortable: true, filter: true },
+    { field: 'customer', sortable: true, filter: true },
+    
+    // Lisää harjoitusten tietoja tarvittaessa
+   /* {
+      cellRenderer: params => <EditTraining fetchTrainings={fetchTrainings} data={params.data} />,
+      width: 120
+    },
+    {
+      cellRenderer: params => 
+        <Button size="small" onClick={() => deleteTraining(params.data._links.training.href)}>
+          Delete
+        </Button>,
+      width: 120
+    }*/
+  ]);
+
+  const fetchTrainings = () => {
+    fetch("http://traineeapp.azurewebsites.net/api/trainings")
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Virhe tietojen hakemisessa");
+      }
+    })
+    .then(data => {
+      if (Array.isArray(data.content)) {
+        setTrainings(data.content);
+        console.log(data)
+      } else {
+        throw new Error("Haettu data ei ole taulukko");
+      }
+  })
+    .catch(error => {
+      console.error("Virhe:", error);
+  })}
+
+  const deleteTraining = (url) => {
+    if (window.confirm("Are you sure?")) {
+      fetch(url, { method: 'DELETE' })
+        .then(response => {
+          if (response.ok)
+            fetchTrainings();
+          else
+            throw new Error("Error in DELETE: " + response.statusText);
+        })
+        .catch(err => console.error(err));
     }
+  };
 
-    return <h2>Treenit</h2>;
-    // Adasd
-  }
-  export default Traininglist;
-  
+  return (
+    <div className="ag-theme-material" style={{ height: 500 }}>
+      <h2>Harjoituslista</h2>
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowData={trainings}
+        animateRows={true}>
+      </AgGridReact>
+    </div>
+  );
+}
+
+export default TrainingList;
